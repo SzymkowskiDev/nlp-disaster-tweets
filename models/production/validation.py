f9d78fcedb05051cd513c0f73eba7709fa83a9c8
stijm <spcstim@gmail.com>
2022-07-28 14:50:25 +0200

    Pass ready instance of estimator instead of passing the class to report generator

--- models/production/validation.py
+++ models/production/validation.py
@@ -1,5 +1,5 @@
 import datetime
-from typing import Any, Iterable, Type
+from typing import Any, Iterable
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -11,18 +11,19 @@ def generate_validation_report(
     X: Iterable, y: Iterable,  # training vectors and target values (class labels)
     name: str,
     description: str,
-    estimator: Type[Any] = svm.SVC,  # classification, by default `sklearn.svm.SVC`
+    clf: Any = None,  # classification instance, by default `sklearn.svm.SVC()`
     test_size: float = .15,
     date_fmt="%Y-%m-%d %H:%M:%S",
 ) -> pd.DataFrame:
     """Generate report of certain model metrics, returned as `pandas.DataFrame`."""
     date = datetime.datetime.now().strftime(date_fmt)
 
-    clf = estimator()
+    clf = clf or svm.SVC()
+
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size)
 
     model = clf.fit(X_train, y_train)
-    predictions = model.predict(X_test)
+    predictions = model.predict(X_test)  # type: ignore
 
     # Compute and return efficiency metrics in a DataFrame, in human-readable format.
     return pd.DataFrame.from_dict({
