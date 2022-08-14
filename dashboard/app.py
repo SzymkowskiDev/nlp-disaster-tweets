85805e5521a46f5244053b5334c1892f770951c0
Kamil Szymkowski <szymkowski.k@gmail.com>
2022-08-14 20:04:42 +0200

    Sections for EDA in the dashboard

--- dashboard/app.py
+++ dashboard/app.py
@@ -4,16 +4,39 @@ import dash_bootstrap_components as dbc
 
 app = Dash(external_stylesheets=[dbc.themes.SLATE, dbc.icons.BOOTSTRAP])
 
+### TAB 1: EXPLORATORY DATA ANALYSIS
 tab1_content = dbc.Card(
     dbc.CardBody(
         [
-            html.P("This is tab 2!", className="card-text"),
-            dbc.Button("Don't click here", color="danger"),
+            html.H2("Introduction"),
+            html.P("We have at our disposal multivariate data consisting of 3 potential explonatory variables (keyword, location, text) and response variable (target). Variables 'keyword', 'location' and 'text' are nominal. The outcome variable 'target' is of binary type."),
+            html.P("In total we have a sample of 10,000."),
+
+            html.H2("Data Quality Issues"),
+            html.P("Missing values are found in the column 'keyword', where there were only 61 missing records (out of 10,000) and in the column 'location', where a total of 2533 values were missing (over 25%)."),
+            
+            html.H2("Keyword"),
+            html.P("Description of variable 'keyword'."),
+            html.P("WORDCLOUD"),
+            
+            html.H2("Location"),
+            html.P("In the variable 'location' there are 3341 distinct values."),
+            html.P("INTERACTIVE MAP"),
+
+            html.H2("Text"),
+            html.P("Description of variable 'text'"),
+            html.P("WORDCLOUD"),
+
+            html.H2("Dataset Balance"),
+            html.P("Is the dataset balanced?"),
+            html.P("BAR CHART"),
+       
         ]
     ),
     className="mt-3",
 )
 
+# TAB 2: CLASSIFICATION
 tab2_content = dbc.Card(
     dbc.CardBody(
     [
@@ -66,7 +89,8 @@ tab2_content = dbc.Card(
     className="mt-3",
 )
 
-tab5_content = dbc.Card(
+# TAB 6: ABOUT
+tab6_content = dbc.Card(
     dbc.CardBody(
         [
             html.P("This is tab 2!", className="card-text"),
@@ -84,9 +108,9 @@ tabs = dbc.Tabs(
         dbc.Tab("This tab's content is never seen", label="Custom data upload", disabled=True, tab_id="tab-3"),
         dbc.Tab("This tab's content is never seen", label="Twitter API Calls", disabled=True, tab_id="tab-4"),
         dbc.Tab("This tab's content is never seen", label="Community labelling", disabled=True, tab_id="tab-5"),
-        dbc.Tab(tab5_content, label="About", tab_id="tab-6")
+        dbc.Tab(tab6_content, label="About", tab_id="tab-6")
     ],
-    active_tab="tab-2"
+    active_tab="tab-1"
 )
 
 ### LAYOUT
