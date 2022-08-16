32a89a3027f8c450ccb9f21600d38d82eaae8f80
Kamil Szymkowski <szymkowski.k@gmail.com>
2022-08-16 13:59:22 +0200

    Combine preprocessing and outputs branches

--- dashboard/app.py
+++ dashboard/app.py
@@ -10,12 +10,14 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from models.production.generate_perf_report import generate_perf_report
 from models.production.vectorize_data import vectorize_data
 import plotly.figure_factory as ff
-from dash import Dash, html
+from turtle import width
+from dash.dependencies import Input, Output
+import plotly.graph_objects as go
 
 
-app = Dash(external_stylesheets=[dbc.themes.SLATE, dbc.icons.BOOTSTRAP])
+app = Dash(external_stylesheets=[dbc.themes.VAPOR, dbc.icons.BOOTSTRAP])
 
-### TAB 1: EXPLORATORY DATA ANALYSIS
+# TAB 1: EXPLORATORY DATA ANALYSIS ###################################################################################################
 tab1_content = dbc.Card(
     dbc.CardBody(
         [
@@ -45,112 +47,85 @@ tab1_content = dbc.Card(
     className="mt-3",
 )
 
-
-# TAB 2: CLASSIFICATION
+# TAB 2: CLASSIFICATION ##############################################################################################################
 tab2_content = dbc.Card(
     dbc.CardBody(
         [
-            dbc.Row(
-                [
-                    dbc.Col(
+            dbc.Row([
+                html.H2("INPUTS"),
+                dbc.Col([html.Div([
+                    html.H3("Data Cleaning", style={"fontSize": 20}),
+                    dcc.Checklist(
                         [
-                            html.Div(
-                                [
-                                    html.H3("Preprocessing", style={"fontSize": 20}),
-                                    dcc.Checklist(
-                                        [
-                                            "Remove hashes",
-                                            "Remove duplicates",
-                                            "Translate emojis",
-                                            # TODO: add more preprocessing ideas
-                                        ],
-                                        value=["Remove hashes", "Remove duplicates"],
-                                        labelStyle={"display": "block"},
-                                        style={
-                                            "height": 200,
-                                            "width": 200,
-                                            "overflow": "auto",
-                                        },
-                                        inputStyle={"marginRight": "12px"},
-                                        id="preprocessing-checklist",
-                                    ),
-                                ]
-                            )
+                            "Remove hashes",
+                            "Remove duplicates",
+                            "Translate emojis",
+                            # TODO: add more preprocessing ideas
                         ],
-                        width=2,
-                    ),
-                    dbc.Col(
+                        value=["Remove hashes",
+                               "Remove duplicates"],
+                        labelStyle={"display": "block"},
+                        style={
+                            "height": 200,
+                            "width": 200,
+                            "overflow": "auto",
+                        },
+                        inputStyle={"marginRight": "12px"},
+                        id="preprocessing-checklist",
+                    )
+                ])], width=4),
+                dbc.Col([html.Div([
+                    html.H3("Vectorization", style={"fontSize": 20}),
+                    dcc.RadioItems(
+                        ["Count", "TF-IDF"],
+                        value="TF-IDF",
+                        labelStyle={"display": "block"},
+                        style={
+                            "height": 200,
+                            "width": 200,
+                            "overflow": "auto",
+                        },
+                        inputStyle={"marginRight": "12px"},
+                        id="vectorization-radio-items",
+                    )
+                ])], width=4),
+                dbc.Col([html.Div([
+                    html.H3("Model", style={"fontSize": 20}),
+                    dcc.RadioItems(
                         [
-                            html.Div(
-                                [
-                                    html.H3("Vectorization", style={"fontSize": 20}),
-                                    dcc.RadioItems(
-                                        ["Count", "TF-IDF"],
-                                        value="TF-IDF",
-                                        labelStyle={"display": "block"},
-                                        style={
-                                            "height": 200,
-                                            "width": 200,
-                                            "overflow": "auto",
-                                        },
-                                        inputStyle={"marginRight": "12px"},
-                                        id="vectorization-radio-items",
-                                    ),
-                                ]
-                            )
+                            "SVC",
+                            "Logistic",
+                            # TODO: add other classifiers
                         ],
-                        width=2,
+                        value="SVC",
+                        labelStyle={"display": "block"},
+                        style={
+                            "height": 200,
+                            "width": 200,
+                            "overflow": "auto",
+                        },
+                        inputStyle={"marginRight": "12px"},
+                        id="model-radio-items",
                     ),
-                    dbc.Col(
-                        [
-                            html.Div(
-                                [
-                                    html.H3("Model", style={"fontSize": 20}),
-                                    dcc.RadioItems(
-                                        [
-                                            "SVC",
-                                            "Logistic",
-                                            # TODO: add other classifiers
-                                        ],
-                                        value="SVC",
-                                        labelStyle={"display": "block"},
-                                        style={
-                                            "height": 200,
-                                            "width": 200,
-                                            "overflow": "auto",
-                                        },
-                                        inputStyle={"marginRight": "12px"},
-                                        id="model-radio-items",
-                                    ),
-                                    dbc.Button(
-                                        "Run", color="success", className="me-1"
-                                    ),
-                                ]
-                            )
-                        ],
-                        width=2,
-                    ),
-                    dbc.Col(
-                        [
-                            html.H2("OUTPUTS"),
-                            html.P(
-                                "Your customized classification correctly predicted 732 responses out of 1002, which ammounts to the accuracy rate of 0.81. Other metrics are shown below."
-                            ),
-                            html.H3(
-                                "Fig 1. Performance Metrics", style={"fontSize": 20}
-                            ),
-                            html.Div(id="output-datatable"),
-                            dcc.Store(id="intermediate-value"),
-                            html.H3("Fig 2. Confusion Matrix", style={"fontSize": 20}),
-                            dcc.Graph(id="confusion-matrix-graph"),
-                            html.H3("Fig 3. ROC & AUC", style={"fontSize": 20}),
-                            dcc.Graph(id="roc-graph"),
-                            html.Div(id="roc-info"),
-                        ],
-                        width=6,
-                    ),
-                ]
-            )
+                    dbc.Button("Run", color="success", className="me-1")
+                ])], width=4)
+            ]),
+            dbc.Row([
+                html.H2("OUTPUTS"),
+                dbc.Col([html.P(
+                    "Your customized classification correctly predicted 732 responses out of 1002, which amounts to the accuracy rate of 0.81. Other metrics are shown below."),
+                    html.H3("Fig 1. Performance Metrics",
+                            style={"fontSize": 20}),
+                    html.Div([
+                        html.Div(id="output-datatable"),
+                        dcc.Store(id="intermediate-value"),
+                        html.P('Run generated on 2022-07-29 00:17:16')
+                    ])], width=4),
+                dbc.Col([html.H3("Fig 2. Confusion Matrix", style={"fontSize": 20}),
+                         dcc.Graph(id="confusion-matrix-graph")], width=4),
+                dbc.Col([html.H3("Fig 3. ROC & AUC", style={"fontSize": 20}),
+                         dcc.Graph(id="roc-graph")], width=4)
+            ]),
         ]
     ),
     className="mt-3",
@@ -188,14 +163,18 @@ def update_datatable(data):
                     html.Tbody(
                         [
                             html.Tr(
-                                [html.Th("Accuracy  "), html.Th(dff.get("Accuracy"))]
+                                [html.Th("Accuracy  "), html.Th(
+                                    dff.get("Accuracy"))]
                             ),
                             html.Tr(
-                                [html.Th("Precision  "), html.Th(dff.get("Precision"))]
+                                [html.Th("Precision  "), html.Th(
+                                    dff.get("Precision"))]
                             ),
-                            html.Tr([html.Th("Recall  "), html.Th(dff.get("Recall"))]),
                             html.Tr(
-                                [html.Th("F1 Score  "), html.Th(dff.get("F1 Score"))]
+                                [html.Th("Recall  "), html.Th(dff.get("Recall"))]),
+                            html.Tr(
+                                [html.Th("F1 Score  "), html.Th(
+                                    dff.get("F1 Score"))]
                             ),
                         ]
                     )
@@ -204,14 +183,10 @@ def update_datatable(data):
                     textAlign="left",
                     padding="5px",
                     border="1px solid grey",
-                    backgroundColor="#1E1E1E",
+                    backgroundColor="#3F3B96",
                     fontWeight="bold",
                     color="white",
-                ),
-                # style_header=dict(
-                #     backgroundColor="#1E1E1E", fontWeight="bold", color="white"
-                # ),
-                # style_data=dict(backgroundColor="#323232", color="white"),
+                )
             )
         ]
     )
@@ -220,7 +195,8 @@ def update_datatable(data):
 
 
 @app.callback(
-    Output("confusion-matrix-graph", "figure"), Input("intermediate-value", "data")
+    Output("confusion-matrix-graph",
+           "figure"), Input("intermediate-value", "data")
 )
 def update_confusion_matrix(data):
     dff = pd.read_json(data, typ="series")
@@ -233,74 +209,53 @@ def update_confusion_matrix(data):
 
     # set up figure
     conf_matrix = ff.create_annotated_heatmap(
-        z,
-        x=x,
-        y=y,
-        annotation_text=z_text,
-        colorscale="greys",
-        font_colors=["red", "red"],
-    )
+        z, x=x, y=y, annotation_text=z_text, colorscale='blues', font_colors=["red", "red"])
 
     # add custom xaxis title
-    conf_matrix.add_annotation(
-        dict(
-            font=dict(color="white", size=18),
-            x=0.5,
-            y=-0.15,
-            showarrow=False,
-            text="Predicted value",
-            xref="paper",
-            yref="paper",
-        )
-    )
+    conf_matrix.add_annotation(dict(font=dict(color="white", size=18),
+                                    x=0.5,
+                                    y=-0.15,
+                                    showarrow=False,
+                                    text="Predicted value",
+                                    xref="paper",
+                                    yref="paper"))
 
     # add custom yaxis title
-    conf_matrix.add_annotation(
-        dict(
-            font=dict(color="white", size=18),
-            x=-0.15,
-            y=0.5,
-            showarrow=False,
-            text="Real value",
-            textangle=-90,
-            xref="paper",
-            yref="paper",
-        )
-    )
+    conf_matrix.add_annotation(dict(font=dict(color="white", size=18),
+                                    x=-0.15,
+                                    y=0.5,
+                                    showarrow=False,
+                                    text="Real value",
+                                    textangle=-90,
+                                    xref="paper",
+                                    yref="paper"))
 
     # adjust margins to make room for yaxis title
     conf_matrix.update_layout(
         margin=dict(t=0, l=50),
-        height=500,
-        width=500,
-        paper_bgcolor="rgba(0,0,0,0)",
-        plot_bgcolor="rgba(0,0,0,0)",
-        font=dict(family="Courier New, monospace", size=22, color="white"),
-    )
+        height=350,
+        width=350,
+        paper_bgcolor='rgba(0,0,0,0)',
+        plot_bgcolor='rgba(0,0,0,0)',
+        font=dict(
+            family="Courier New, monospace",
+            size=22,
+            color="white"
+        ))
+
     # add colorbar
-    conf_matrix["data"][0]["showscale"] = True
+    conf_matrix['data'][0]['showscale'] = True
+
     # TODO: complete this function
     return conf_matrix
-
-
-# TAB 6: ABOUT
-tab6_content = dbc.Card(
-    dbc.CardBody(
-        [
-            html.P("This is tab 2!", className="card-text"),
-            dbc.Button("Don't click here", color="danger"),
-        ]
-    ),
-    className="mt-3",
-)
-
 
 @app.callback(Output("roc-graph", "figure"), Input("intermediate-value", "data"))
 def update_roc(data):
     dff = pd.read_json(data, typ="series")
     fpr, tpr, _ = dff.get("Roc curve")
     fig = px.area(
-        x=fpr, y=tpr, labels=dict(x="False Positive Rate", y="True Positive Rate")
+        x=fpr, y=tpr, labels=dict(
+            x="False Positive Rate", y="True Positive Rate")
     )
     fig.add_shape(
         type="line", line=dict(dash="dash", color="red"), x0=0, x1=1, y0=0, y1=1
@@ -311,31 +266,41 @@ def update_roc(data):
 
     fig.update_layout(
         margin=dict(t=10, l=50),
-        height=500,
-        width=500,
-        paper_bgcolor="rgba(0,0,0,0)",
-        plot_bgcolor="rgba(0,0,0,0)",
-        font=dict(family="Courier New, monospace", size=18, color="white"),
-    )
+        height=350,
+        width=450,
+        paper_bgcolor='rgba(0,0,0,0)',
+        plot_bgcolor='rgba(0,0,0,0)',
+        font=dict(
+            family="Courier New, monospace",
+            size=18,
+            color="white"
+        ))
+
+    fig.add_trace(go.Scatter(
+        x=[0.7],
+        y=[0.3],
+        mode="text",
+        text=[f'AUC = {auc(fpr, tpr):.4f}'],
+        textposition="bottom center"
+    ))
+
+    fig.update_layout(showlegend=False)
+
     return fig
 
 
-@app.callback(Output("roc-info", "children"), Input("intermediate-value", "data"))
-def update_roc_info(data):
-    dff = pd.read_json(data, typ="series")
-    roc_auc_score = dff.get("Roc_auc_score")
-    return (
-        html.P(
-            f"AUC = {roc_auc_score:.4f}",
-            style={
-                "fontFamily": "Courier New, monospace",
-                "fontSize": 20,
-                "color": "red",
-            },
-            id="roc-paragraph",
-        ),
-    )
+# TAB 7: ABOUT ######################################################################################################################
+tab6_content = dbc.Card(
+    dbc.CardBody(
+        [
+            html.P("This is tab 2!", className="card-text"),
+            dbc.Button("Don't click here", color="danger"),
+        ]
+    ),
+    className="mt-3",
+)
 
+# TABS SETUP ########################################################################################################################
 
 tabs = dbc.Tabs(
     [
@@ -343,28 +308,34 @@ tabs = dbc.Tabs(
         dbc.Tab(tab2_content, label="Classification", tab_id="tab-2"),
         dbc.Tab(
             "This tab's content is never seen",
-            label="Custom data upload",
+            label="Compare runs",
             disabled=True,
             tab_id="tab-3",
         ),
         dbc.Tab(
             "This tab's content is never seen",
-            label="Twitter API Calls",
+            label="Custom data upload",
             disabled=True,
             tab_id="tab-4",
         ),
         dbc.Tab(
             "This tab's content is never seen",
-            label="Community labelling",
+            label="Twitter API Calls",
             disabled=True,
             tab_id="tab-5",
         ),
-        dbc.Tab(tab6_content, label="About", tab_id="tab-6"),
+        dbc.Tab(
+            "This tab's content is never seen",
+            label="Community labelling",
+            disabled=True,
+            tab_id="tab-6",
+        ),
+        dbc.Tab(tab6_content, label="About", tab_id="tab-7"),
     ],
     active_tab="tab-2",
 )
 
-### LAYOUT
+# LAYOUT ##############################################################################################################################
 app.layout = dbc.Container(
     [html.H1([html.I(className="bi bi-twitter me-2"), "NLP Disaster Tweets"]), tabs]
 )
