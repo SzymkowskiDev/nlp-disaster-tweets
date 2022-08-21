7c796d429c84e94f04db695a5c77d1895507a1d9
Oblak <oblako.tka@gmail.com>
2022-08-21 15:41:58 +0200

    add comments to word-freq.py

--- data/word_frequencies/word-freq.py
+++ data/word_frequencies/word-freq.py
@@ -3,13 +3,15 @@ import sys
 import pandas as pd
 from collections import Counter
 
-
 CLEAR_PATH = r'data/clear/'
 FREQ_PATH = r'data/word_frequencies/'
 
 
 
 def get_corpus_df(df:pd.DataFrame) -> list:
+    """
+    Reciwes df and returns a list with words that are in the tweets.
+    """
     words = []
     
     for row in df:
@@ -20,6 +22,9 @@ def get_corpus_df(df:pd.DataFrame) -> list:
 
 
 def get_corpus_str(text: str) -> list:
+    """
+    Reciwes str and returns a list with words that are in the tweets.
+    """
     words = []
     for i in text:
         i = str(i)
@@ -28,11 +33,15 @@ def get_corpus_str(text: str) -> list:
     return words
 
 
-# Get names of files in CLEAR_PATH
+# Get names of files in CLEAR_PATH.
 for (dirpath, dirnames, filenames) in walk(CLEAR_PATH):
+
+    # outputs all possible filenames from CLEAR_PATH folder
     if(len(filenames) != 0):
         print('The folowing files were found:')
         print(filenames)
+    
+    # if there are no files in the CLEAR_PATH folder, scrypt terminates
     else:
         print(f"There are no files in the {CLEAR_PATH} path. ;(")
         print("Exit...\n")
@@ -57,7 +66,7 @@ while True:
         continue
 
 
-# Try to read column
+# Try to read column.
 while True:
     list_columns = list(df.columns[3:])
 
@@ -75,33 +84,32 @@ while True:
         print("Wrong name of column, try again.")
         continue
 
-    # Read as df if ok
+    # Read as df if ok.
     try:
         eval(df[column][0])
         corpus = get_corpus_df(df[column])
     
-    # read as str if not ok
+    # read as str if not ok.
     except SyntaxError:
         corpus = get_corpus_str(df[column])
 
     break
 
 
-# Word frequency counter in tweets
+# Word frequency counter in tweets.
 counter = Counter(corpus)
 
-# Convert to DF and add name to columns
+# Convert to DF and add name to columns.
 freq_df =  pd.DataFrame.from_dict(counter, orient='index')
 freq_df['word'] = freq_df.index
 freq_df['freq'] = freq_df[0]
 freq_df.drop(0, axis=1, inplace=True)
 freq_df.reset_index(drop=True, inplace=True)
 
-# Sort by counter
+# Sort by column *freq* in descending order(From larger to smaller).
 freq_df.sort_values('freq', inplace=True, ascending=[False])
 
-print(freq_df.head())
-# Save
+# Save 2 columns(word, freq) df with *,* separator. Save to FREQ_PATH folder.
 FREQ_NAME = f'word-freq_{column}.csv'
 freq_df.to_csv(FREQ_PATH+FREQ_NAME, index=False)
 
