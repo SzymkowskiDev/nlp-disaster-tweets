aeb698d1bbde9831633f709a3869807f703731fc
bswck <bswck.dev@gmail.com>
2022-09-04 01:04:46 +0200

    Fix db builder

--- dashboard/src/data/location/normalization/build_geonamebase.py
+++ dashboard/src/data/location/normalization/build_geonamebase.py
@@ -1,11 +1,15 @@
 # note: you must run this file in order to get the geoname database,
 # that is needed to use search() function later
+import logging
+import sys
 import time
 
 from dashboard.src.data.location.normalization import geonamebase
 
 
 def build_geonamebase(path=geonamebase.DEFAULT_PATH):
+    if "-v" in sys.argv:
+        geonamebase.log.setLevel(logging.INFO)
     geonamebase.log.info(f"Building your geographical name database at {path}...")
     db = geonamebase.get_geonamebase(_flag="c")
     start = time.perf_counter()
